0dcfca561f42e55afac2f0cdda7b93d99b4f65fc
Markiizioni <csanojr@gmail.com>
2025-04-03 22:24:37 +0400

    Add: cloned Java programs for various functionalities from given repository

---
+++ microservice-clone-and-process/repositories/java/tmp2av_uagz_T1.java
@@ -0,0 +1,9 @@
+public class t1 {
+public static void main(string[] args) {
+system.out.println("welcome to java");
+system.out.println("welcome to java");
+system.out.println("welcome to java");
+system.out.println("welcome to java");
+system.out.println("welcome to java");
+}
+}
