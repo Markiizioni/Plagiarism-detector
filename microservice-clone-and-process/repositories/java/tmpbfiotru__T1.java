54850b25239d0313949dc7f2f7aa48416d30def6
Markiizioni <csanojr@gmail.com>
2025-04-04 16:57:43 +0400

    Remove obsolete Java files from the microservice clone and process repository

--- microservice-clone-and-process/repositories/java/tmpbfiotru__T1.java
+++
@@ -1,9 +0,0 @@
-public class t1 {
-public static void main(string[] args) {
-system.out.println("welcome to java");
-system.out.println("welcome to java");
-system.out.println("welcome to java");
-system.out.println("welcome to java");
-system.out.println("welcome to java");
-}
-}
